4066ad4f7f926a76ccd206c733d83c3780d06429
Marco Lindner <marco.lindner@gmx.org>
2023-11-13 18:07:50 +0100

    Fixing file check

--- add_numbers/test_add_numbers.py
+++ add_numbers/test_add_numbers.py
@@ -11,8 +11,11 @@ from add_numbers import add_two_numbers
 
 class TestAddTwoNumbers(unittest.TestCase):
     def test_file_exists(self):
+        current_file = Path(__file__).resolve()
+        parent_dir = current_file.parents[2]
         file_name = self.__module__.split(".")[-1] + ".py"
-        self.assertTrue(parent_dir.is_file(file_name), f"{file_name} does not exist")
+        file_path = parent_dir / file_name
+        self.assertTrue(file_path.is_file(), f"{file_name} does not exist")
 
     def test_addition(self):
         self.assertEqual(add_two_numbers(1, 2), 3)
--- number_is_even/test_is_even.py
+++ number_is_even/test_is_even.py
@@ -11,7 +11,7 @@ from is_even import is_even
 
 class TestIsEven(unittest.TestCase):
     def test_file_exists(self):
-        self.assertTrue(os.path.isfile("is_even.py"), "is_even.py does not exist")
+        self.assertTrue(os.path.dirname.isfile("is_even.py"), "is_even.py does not exist")
 
     def test_is_even(self):
         self.assertTrue(is_even(2))
