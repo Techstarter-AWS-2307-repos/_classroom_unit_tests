675a78a6fa75c47dd23486c3f9390319e64ecd7d
Marco Lindner <marco.lindner@gmx.org>
2023-11-13 17:38:50 +0100

    Adding Modules

--- number_is_even/test_is_even.py
+++ number_is_even/test_is_even.py
@@ -1,5 +1,11 @@
 import unittest
 import os
+import sys
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parents[2]
+sys.path.append(str(parent_dir))
+
 from is_even import is_even
 
 
